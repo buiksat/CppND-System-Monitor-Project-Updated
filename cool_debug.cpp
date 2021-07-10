44d813a93837e46313476df27749b95fb45be50e
Bahram Behzadian <zodiac_se@yahoo.com>
2021-07-10 13:33:01 -0400

    add cool debug

---
+++ cool_debug.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#define printVariableNameAndValue(x) cout<<"The name of variable **"<<(#x)<<"** and the value of variable is => "<<x<<"\n"
+// I am talking about this piece of code ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+using namespace std;
+
+
+int main(){
+    int n = 25;
+    string myFirstStringEver = "Hello World";
+
+    printVariableNameAndValue(n);
+    printVariableNameAndValue(myFirstStringEver);
+
+}
